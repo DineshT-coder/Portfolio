2281d10d9e589edb390df4a1f4f0262599762460
Dinesh Dharshan T <124598517+DineshT-coder@users.noreply.github.com>
2025-03-05 00:53:59 +0530

    Add files via upload

---
+++ js/certifications.js
@@ -0,0 +1,38 @@
+// Smooth scrolling for navigation links
+$(document).ready(function(){
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    // Dynamic text effect
+    const texts = ["Knowledge is Power","Learning is a Treasure","Education is the Key to Success"];
+    let count = 0;
+    let index = 0;
+    let currentText = "";
+    let letter = "";
+
+    (function type() {
+        if (count === texts.length) {
+            count = 0;
+        }
+        currentText = texts[count];
+        letter = currentText.slice(0, ++index);
+
+        document.getElementById('dynamic-text').textContent = letter;
+        if (letter.length === currentText.length) {
+            count++;
+            index = 0;
+            setTimeout(type, 2000);  // Pause for 2 seconds before typing the next text
+        } else {
+            setTimeout(type, 150);
+        }
+    })();
+});
---
+++ js/skillset.js
@@ -0,0 +1,38 @@
+// Smooth scrolling for navigation links
+$(document).ready(function(){
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    // Dynamic text effect
+    const texts = ["First, solve the problem-Then, write the code","The beautiful thing about learning is that no one can take it away from you","Your skills are your superpowers-keep upgrading them"];
+    let count = 0;
+    let index = 0;
+    let currentText = "";
+    let letter = "";
+
+    (function type() {
+        if (count === texts.length) {
+            count = 0;
+        }
+        currentText = texts[count];
+        letter = currentText.slice(0, ++index);
+
+        document.getElementById('dynamic-text').textContent = letter;
+        if (letter.length === currentText.length) {
+            count++;
+            index = 0;
+            setTimeout(type, 2000);  // Pause for 2 seconds before typing the next text
+        } else {
+            setTimeout(type, 150);
+        }
+    })();
+});
